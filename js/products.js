9a2fb70c76acc9cf1052dd480135ab23c910e960
Ori Baram <obrm770@gmail.com>
2023-01-22 11:43:17 +0200

    first commit

---
+++ js/products.js
@@ -0,0 +1,58 @@
+const data = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro Max",
+    category: "Smartphones",
+    price: "$999.99",
+    rating: 4.8,
+  },
+  {
+    id: 2,
+    name: "Samsung Galaxy S21 Ultra",
+    category: "Smartphones",
+    price: "$1299.99",
+    rating: 4.6,
+  },
+  {
+    id: 3,
+    name: "MacBook Pro 16-inch",
+    category: "Laptops",
+    price: "$2499.99",
+    rating: 4.9,
+  },
+  {
+    id: 4,
+    name: "Dell XPS 15",
+    category: "Laptops",
+    price: "$1299.99",
+    rating: 4.7,
+  },
+  {
+    id: 5,
+    name: "Sony A7 III",
+    category: "Cameras",
+    price: "$1999.99",
+    rating: 4.9,
+  },
+  {
+    id: 6,
+    name: "Canon EOS R5",
+    category: "Cameras",
+    price: "$3899.99",
+    rating: 4.8,
+  },
+  {
+    id: 7,
+    name: "Bose QuietComfort 35 II",
+    category: "Headphones",
+    price: "$349.99",
+    rating: 4.7,
+  },
+  {
+    id: 8,
+    name: "Beats Studio 3 Wireless",
+    category: "Headphones",
+    price: "$349.99",
+    rating: 4.5,
+  }
+];

4f4686cb558f2c9ff09f21736469292e565bffaf
Saleh Kalouti <salehkalouti@gmail.com>
2023-01-23 12:12:03 +0200

    basic structure

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const items = [
+    {
+        id: 1,
+        task: "buy milk",
+        done: false,
+        priority: 0,
+        dueDate: new Date
+    }
+]
+
+const chooseAction = input => {
+    // 1- addItem 
+    // 2 - remove 
+    // ...
+
+    // 1// input type is a number
+}
+
+
+
+
+
+
+
+const addItem = input => { }
+const markAsDone = input => { }
+const removeItem = input => { }
+const displayItems = input => { }
+const sortBy = input => { }
+const search = input => { }
